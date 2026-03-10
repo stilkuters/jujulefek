e62d3c53721a2b5cf502727b3ff186ba430be1ce
stilkuters <stilkuters@quickmails.eu>
2026-03-10 09:08:57 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,14 +1,9 @@
 #!/bin/bash
 
-HOST=$(cat hostname.txt)
-
 echo "Starting TCP proxy..."
 
 socat TCP-LISTEN:443,fork TCP:dagnam.xyz:4629 &
 
 echo "Starting Cloudflare tunnel..."
 
-cloudflared tunnel \
---no-autoupdate \
---url tcp://127.0.0.1:443 \
---hostname $HOST
+cloudflared tunnel run --token $TUNNEL_TOKEN
